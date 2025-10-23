c4d4291fcaa32a25ae443d94bba57c870674167f
Muzami <muzamilhussain072005@gmail.com>
2025-10-23 14:01:02 +0200

    blablabla

--- git_test.py
+++ git_test.py
@@ -1,7 +1,7 @@
 # This is to test my github connection with VS Code
-print("I am uploading this file to GitHub from VS Code")
+print("Are you uploading this file to GitHub from VS Code")
 name = input("Enter your name: ")
-print(f"hello, {name}. How can i help you today?")
+print(f"hello, {name}. Is there anything else that i can help you today?")
 
 # i have to upload this to github but an interesting task is to do the same
 # in your terma
