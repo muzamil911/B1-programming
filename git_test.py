c8a1a2f6b1faa6256e584499724db9e4eff1f713
Muzami <muzamilhussain072005@gmail.com>
2025-10-23 13:58:18 +0200

    revision

--- git_test.py
+++ git_test.py
@@ -1,2 +1,7 @@
 # This is to test my github connection with VS Code
 print("I am uploading this file to GitHub from VS Code")
+name = input("Enter your name: ")
+print(f"hello, {name}. How can i help you today?")
+
+# i have to upload this to github but an interesting task is to do the same
+# in your terma
