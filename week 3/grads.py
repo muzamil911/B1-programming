ca43b0a448270f4d1e5511808f38b0b5b922bf3d
Muzami <muzamilhussain072005@gmail.com>
2025-10-28 11:25:30 +0100

    week 3 update

---
+++ week 3/grads.py
@@ -0,0 +1,17 @@
+
+
+# asking for score:
+score = input("Enter your score (0-100): ")
+
+if score >= '90':
+    print("Your grade is 'A'")
+elif score >= '80':
+    print("Your grade is 'B'")
+elif score >= '70':
+    print("Your grade is 'C'.")
+elif score >= '60':
+    print("Your grade is 'D'.")
+else:
+    print("Your grade is 'F'.")
+
+    
